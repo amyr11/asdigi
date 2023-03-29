fb68cd7da912831655d065d6bdd04886761a4312
Amyr Francisco <amyrfrancisco11@gmail.com>
2023-03-29 21:26:28 +0800

    created separate stful widget for checklist section to avoid rebuilding

---
+++ lib/components/milestone_checklist_section.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../models/milestone.dart';
+import 'milestone_checklist_item_card.dart';
+
+class MilestoneCheclistSection extends StatefulWidget {
+  final List<MilestoneChecklistItem> milestones;
+
+  const MilestoneCheclistSection(this.milestones, {super.key});
+
+  @override
+  State<MilestoneCheclistSection> createState() =>
+      _MilestoneCheclistSectionState();
+}
+
+class _MilestoneCheclistSectionState extends State<MilestoneCheclistSection>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: widget.milestones.length,
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: MilestoneCheclistItemCard(
+              imageURL: widget.milestones[index].imageURL,
+              title: widget.milestones[index].title,
+              onChangeSelected: (value) {
+                setState(() {
+                  widget.milestones[index].status = value;
+                });
+              },
+            ),
+          );
+        },
+      ),
+    );
+    ;
+  }
+}
--- lib/screens/milestones_checklist.dart
+++ lib/screens/milestones_checklist.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../components/milestone_checklist_item_card.dart';
+import '../components/milestone_checklist_section.dart';
 import '../models/milestone.dart';
 import '../temp/temp_data.dart';
 
@@ -12,9 +13,7 @@ class MilestonesChecklistPage extends StatefulWidget {
       _MilestonesChecklistPageState();
 }
 
-class _MilestonesChecklistPageState extends State<MilestonesChecklistPage>
-    with SingleTickerProviderStateMixin {
-  late TabController tabController;
+class _MilestonesChecklistPageState extends State<MilestonesChecklistPage> {
   late List<MilestoneChecklistItem> socialMilestones;
   late List<MilestoneChecklistItem> languageMilestones;
   late List<MilestoneChecklistItem> cognitiveMilestones;
@@ -23,8 +22,6 @@ class _MilestonesChecklistPageState extends State<MilestonesChecklistPage>
   @override
   void initState() {
     super.initState();
-    tabController = TabController(length: 4, vsync: this);
-
     socialMilestones = MilestonesCheclistPageData().socialMilestones;
     languageMilestones = MilestonesCheclistPageData().languageMilestones;
     cognitiveMilestones = MilestonesCheclistPageData().cognitiveMilestones;
@@ -32,162 +29,67 @@ class _MilestonesChecklistPageState extends State<MilestonesChecklistPage>
   }
 
   @override
-  void dispose() {
-    tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          SliverAppBar(
-            floating: true,
-            snap: true,
-            pinned: true,
-            toolbarHeight: 130,
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Juan (5 years old)'),
-                const Text('Milestones Checklist'),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.zero,
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    'See all Developmental Milestones',
-                    style: TextStyle(
-                      decoration: TextDecoration.underline,
+  Widget build(BuildContext context) => DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: NestedScrollView(
+            headerSliverBuilder: (context, innerBoxIsScrolled) => [
+              SliverAppBar(
+                floating: true,
+                snap: true,
+                pinned: true,
+                toolbarHeight: 130,
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Juan (5 years old)'),
+                    const Text('Milestones Checklist'),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'See all Developmental Milestones',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
                     ),
+                  ],
+                ),
+                bottom: const PreferredSize(
+                  preferredSize: Size.fromHeight(50),
+                  child: TabBar(
+                    isScrollable: true,
+                    tabs: [
+                      Tab(
+                        text: 'Social',
+                      ),
+                      Tab(
+                        text: 'Language',
+                      ),
+                      Tab(
+                        text: 'Cognitive',
+                      ),
+                      Tab(
+                        text: 'Movement',
+                      )
+                    ],
+                    indicatorSize: TabBarIndicatorSize.tab,
                   ),
                 ),
+              ),
+            ],
+            body: TabBarView(
+              children: [
+                MilestoneCheclistSection(socialMilestones),
+                MilestoneCheclistSection(languageMilestones),
+                MilestoneCheclistSection(cognitiveMilestones),
+                MilestoneCheclistSection(movementMilestones),
               ],
             ),
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(50),
-              child: TabBar(
-                isScrollable: true,
-                tabs: const [
-                  Tab(
-                    text: 'Social',
-                  ),
-                  Tab(
-                    text: 'Language',
-                  ),
-                  Tab(
-                    text: 'Cognitive',
-                  ),
-                  Tab(
-                    text: 'Movement',
-                  )
-                ],
-                controller: tabController,
-                indicatorSize: TabBarIndicatorSize.tab,
-              ),
-            ),
           ),
-        ],
-        body: TabBarView(
-          controller: tabController,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: socialMilestones.length,
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: MilestoneCheclistItemCard(
-                      imageURL: socialMilestones[index].imageURL,
-                      title: socialMilestones[index].title,
-                      intialSelected: socialMilestones[index].status,
-                      onChangeSelected: (value) {
-                        setState(() {
-                          socialMilestones[index].status = value;
-                        });
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: languageMilestones.length,
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: MilestoneCheclistItemCard(
-                      imageURL: languageMilestones[index].imageURL,
-                      title: languageMilestones[index].title,
-                      intialSelected: languageMilestones[index].status,
-                      onChangeSelected: (value) {
-                        setState(() {
-                          languageMilestones[index].status = value;
-                        });
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: cognitiveMilestones.length,
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: MilestoneCheclistItemCard(
-                      imageURL: cognitiveMilestones[index].imageURL,
-                      title: cognitiveMilestones[index].title,
-                      intialSelected: cognitiveMilestones[index].status,
-                      onChangeSelected: (value) {
-                        setState(() {
-                          cognitiveMilestones[index].status = value;
-                        });
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: movementMilestones.length,
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: MilestoneCheclistItemCard(
-                      imageURL: movementMilestones[index].imageURL,
-                      title: movementMilestones[index].title,
-                      intialSelected: movementMilestones[index].status,
-                      onChangeSelected: (value) {
-                        setState(() {
-                          movementMilestones[index].status = value;
-                        });
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
         ),
-      ),
-    );
-  }
+      );
 }
