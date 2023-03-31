954309b6f81e319c60e33aaaf5a9519f9d6e61ef
Amyr Francisco <amyrfrancisco11@gmail.com>
2023-04-01 01:23:53 +0800

    removed financial and fixed doctors layout

--- lib/screens/doctors.dart
+++ lib/screens/doctors.dart
@@ -53,41 +53,20 @@ class _DoctorsPageState extends State<DoctorsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            toolbarHeight: 0,
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  text: 'Doctors',
-                ),
-                Tab(
-                  text: 'Financial Aid',
-                )
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              Padding(
+    return NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          SliverAppBar(
+            floating: true,
+            pinned: true,
+            title: const Text('Doctors/Therapists'),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(70),
+              child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      'Doctors/Therapists',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
                     Row(
                       children: [
                         Expanded(
@@ -128,66 +107,107 @@ class _DoctorsPageState extends State<DoctorsPage> {
                     const SizedBox(
                       height: 20,
                     ),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: filteredDoctors.length,
-                        itemBuilder: (context, index) {
-                          return CustomDoctorItem(
-                            pfp: filteredDoctors[index].profilePic,
-                            docName: filteredDoctors[index].name,
-                            docOccupation: filteredDoctors[index].occupation,
-                            description: filteredDoctors[index].jobDescription,
-                            address: filteredDoctors[index].address,
-                            trunkLine: filteredDoctors[index].trunkLine,
-                            hospitalName: filteredDoctors[index].hospital,
-                          );
-                        },
-                        shrinkWrap: true,
-                      ),
-                    )
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Text(
-                        'Financial Assistance',
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 20),
-                      child: Text(
-                        'Discover financial help and sponsorships',
-                        style: Theme.of(context).textTheme.titleSmall,
-                      ),
-                    ),
-                    Expanded(
-                      child: ListView.separated(
-                        itemCount: 3,
-                        separatorBuilder: (BuildContext context, int index) {
-                          return const SizedBox(
-                              height:
-                                  10); // set the height of the separator here
-                        },
-                        itemBuilder: (BuildContext context, int index) {
-                          return CustomFinancialItem();
-                        },
-                        shrinkWrap: true,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
+            ),
           ),
+        ];
+      },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView.builder(
+          itemCount: filteredDoctors.length,
+          itemBuilder: (context, index) {
+            return CustomDoctorItem(
+              pfp: filteredDoctors[index].profilePic,
+              docName: filteredDoctors[index].name,
+              docOccupation: filteredDoctors[index].occupation,
+              description: filteredDoctors[index].jobDescription,
+              address: filteredDoctors[index].address,
+              trunkLine: filteredDoctors[index].trunkLine,
+              hospitalName: filteredDoctors[index].hospital,
+            );
+          },
+          shrinkWrap: true,
         ),
       ),
     );
   }
 }
+
+// Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: 16),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           const SizedBox(
+//             height: 20,
+//           ),
+//           Text(
+//             'Doctors/Therapists',
+//             style: Theme.of(context).textTheme.titleLarge,
+//           ),
+//           const SizedBox(
+//             height: 20,
+//           ),
+//           Row(
+//             children: [
+//               Expanded(
+//                 child: CustomDropDown(
+//                     value: textValuePlaces,
+//                     isExpanded: true,
+//                     list: DoctorsPageData.places,
+//                     onChanged: (String? value) {
+//                       setState(
+//                         () {
+//                           textValuePlaces = value!;
+//                           setFilteredDoctors(
+//                               textValuePlaces, textValueOccupation);
+//                         },
+//                       );
+//                     }),
+//               ),
+//               const SizedBox(
+//                 width: 10,
+//               ),
+//               Expanded(
+//                 child: CustomDropDown(
+//                     value: textValueOccupation,
+//                     isExpanded: true,
+//                     list: DoctorsPageData.occupations,
+//                     onChanged: (String? value) {
+//                       setState(
+//                         () {
+//                           textValueOccupation = value!;
+//                           setFilteredDoctors(
+//                               textValuePlaces, textValueOccupation);
+//                         },
+//                       );
+//                     }),
+//               ),
+//             ],
+//           ),
+//           const SizedBox(
+//             height: 20,
+//           ),
+    //       Expanded(
+    //         child: ListView.builder(
+    //           itemCount: filteredDoctors.length,
+    //           itemBuilder: (context, index) {
+    //             return CustomDoctorItem(
+    //               pfp: filteredDoctors[index].profilePic,
+    //               docName: filteredDoctors[index].name,
+    //               docOccupation: filteredDoctors[index].occupation,
+    //               description: filteredDoctors[index].jobDescription,
+    //               address: filteredDoctors[index].address,
+    //               trunkLine: filteredDoctors[index].trunkLine,
+    //               hospitalName: filteredDoctors[index].hospital,
+    //             );
+    //           },
+    //           shrinkWrap: true,
+    //         ),
+    //       )
+    //     ],
+    //   ),
+    // )
